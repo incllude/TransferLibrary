6d66571a0b5dd0340589566935b0bd9cace3a5c3
incllude <binaryio.h@gmail.com>
2023-01-26 15:13:47 +0300

    commit

--- test/sen.cpp
+++ test/sen.cpp
@@ -21,7 +21,6 @@ int main() {
     uint64_t size_of_pack, file_name, safe, port = 4000;
     string ip_address, cur_way = filesystem::current_path().string();
     cur_way = cur_way.substr(0, cur_way.rfind("test") + 4);
-    cout << cur_way << '\n';
 
     vector<uint64_t> available_sizes = {1024, 1048576, 268435456};
     vector<string> available_files = {"archive.zip", "constitution.pdf", "orwell_1984.txt", "kotik.gif"};
